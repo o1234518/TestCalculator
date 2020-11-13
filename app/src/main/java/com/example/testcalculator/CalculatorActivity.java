4fec5f92ad0aabf63829131cecdab6ec4a2d27ea
o1234518 <o1234518@gmail.com>
2020-11-13 19:39:25 +0800

    User name input & intent to next activity finish.

--- app/src/main/java/com/example/testcalculator/CalculatorActivity.java
+++ app/src/main/java/com/example/testcalculator/CalculatorActivity.java
@@ -2,7 +2,9 @@ package com.example.testcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        Intent intent = getIntent();
+        String user_name = intent.getStringExtra("user_name");
+        Toast.makeText(this, "Hi, " + user_name, Toast.LENGTH_LONG).show();
+
     }
 }
--- app/src/main/java/com/example/testcalculator/MainActivity.java
+++ app/src/main/java/com/example/testcalculator/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.testcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputLayout layout_name;
+    private EditText edit_name;
+    private Button btn_nameConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        layout_name.setErrorEnabled(true);
+        btn_nameConfirm.setOnClickListener(clickListner);
     }
+
+    private void init() {
+        layout_name = findViewById(R.id.layout_name);
+        edit_name = findViewById(R.id.edit_name);
+        btn_nameConfirm = findViewById(R.id.btn_name_confirm);
+    }
+
+    private Button.OnClickListener clickListner = new Button.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.btn_name_confirm :
+                    String user_name = edit_name.getText().toString();
+                    if(user_name.length() <= 0) {
+                        layout_name.setError(getResources().getString(R.string.name_error));
+                    } else {
+                        Intent intent = new Intent(getApplicationContext(), CalculatorActivity.class);
+                        intent.putExtra("user_name", user_name);
+                        startActivity(intent);
+                    }
+            }
+        }
+    };
 }
